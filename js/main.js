d7352ed5f9e557717044d105a184d28bbf360afe
theTwister <theTwist84@users.noreply.github.com>
2016-08-24 14:28:37 +0100

    Add support for enter key

--- js/main.js
+++ js/main.js
@@ -2,10 +2,22 @@ var myCodeMirror;
 jQuery(function() {
     myCodeMirror = CodeMirror.fromTextArea(rconConsole);
 
-
-    //TODO: Support up arrow, enter, etc
+    //TODO: Support up arrow, etc
     $("#runCommand").click(function() {
         dewRcon.send(jQuery("#rconCommand").val());
         jQuery("#rconCommand").val("");
+    });
+    $('.panel-center').on('keydown', '#rconCommand', function(e) {
+        var key = e.which;
+        switch (key) {
+            case 13: // enter
+                //alert('Enter key pressed.');
+                dewRcon.send(jQuery("#rconCommand").val());
+                jQuery("#rconCommand").val("");
+                curCommand++;
+                break;
+            default:
+                break;
+        }
     });
 });
